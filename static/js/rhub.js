ec28e9adfd28eda0bf3cfdbf4c8874e628401022
Maëlle Salmon <maelle.salmon@yahoo.se>
2019-11-19 14:54:24 +0100

    more

--- static/js/rhub.js
+++ static/js/rhub.js
@@ -2,7 +2,8 @@
     // Fix Hugo's auto-generated Table of Contents.
     //   Must be performed prior to initializing ScrollSpy.
     $('#TableOfContents > ul > li > ul').unwrap().unwrap();
-    $('#TableOfContents').addClass('nav flex-column');
+    $('#TableOfContents').addClass('nav flex-column sticky');
+    $('#TableOfContents ul').addClass('nav nav-pills nav-stacked');
     $('#TableOfContents li').addClass('nav-item');
     $('#TableOfContents li a').addClass('nav-link');
 });
